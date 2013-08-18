8b49a6c579901f5155799f1151d325b94d7f3ad6
Chicken Bones <mehvids@gmail.com>
2013-08-19 08:04:10 +1000

    Fixed double click crash

--- codechicken/multipart/minecraft/EventHandler.java
+++ codechicken/multipart/minecraft/EventHandler.java
@@ -39,6 +39,9 @@ public class EventHandler
     public static boolean place(EntityPlayer player, World world)
     {
         MovingObjectPosition hit = RayTracer.reTrace(world, player);
+        if(hit == null)
+            return false;
+        
         BlockCoord pos = new BlockCoord(hit.blockX, hit.blockY, hit.blockZ).offset(hit.sideHit);
         ItemStack held = player.getHeldItem();
         McBlockPart part = null;
