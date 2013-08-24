9ea51c5f50b2e11e78f61b61bf18f64573a1436e
Chicken Bones <mehvids@gmail.com>
2013-08-24 18:41:15 +1000

    fix typo that broke Redstone Torches

--- codechicken/multipart/minecraft/RedstoneTorchPart.java
+++ codechicken/multipart/minecraft/RedstoneTorchPart.java
@@ -234,6 +234,6 @@ public class RedstoneTorchPart extends TorchPart implements IFaceRedstonePart, I
     
     @Override
     public int getFace() {
-        return metaSideMap[meta*7];
+        return metaSideMap[meta&7];
     }
 }
