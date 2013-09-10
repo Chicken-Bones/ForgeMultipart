6a57c9cbf1cc2469f83eb129d07587c1551dc520
RainWarrior <rainwarrior@fry.su>
2013-09-10 08:39:33 +0400

    Reverted TCuboidPart to master

--- codechicken/multipart/TCuboidPart.scala
+++ codechicken/multipart/TCuboidPart.scala
@@ -5,6 +5,7 @@ import codechicken.lib.raytracer.IndexedCuboid6
 import net.minecraft.client.renderer.RenderBlocks
 import codechicken.lib.render.CCRenderState
 import codechicken.lib.render.RenderUtils
+import codechicken.lib.vec.Vector3
 import codechicken.lib.render.IconTransformation
 import scala.collection.JavaConversions._
 import java.lang.Iterable
@@ -17,7 +18,7 @@ trait TCuboidPart extends TMultiPart
     def getBounds:Cuboid6
     
     override def getSubParts:Iterable[IndexedCuboid6] = Seq(new IndexedCuboid6(0, getBounds))
-
+    
     override def getCollisionBoxes:Iterable[Cuboid6] = Seq(getBounds)
     
     override def drawBreaking(renderBlocks:RenderBlocks)
