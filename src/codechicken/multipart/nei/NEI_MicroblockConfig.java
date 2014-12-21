81140fb48e01f022c83b825c26b04b131f66e7f0
Chicken-Bones <mehvids@gmail.com>
2014-12-22 03:08:10 +1000

    Added back NEI subsets

--- src/codechicken/multipart/nei/NEI_MicroblockConfig.java
+++ src/codechicken/multipart/nei/NEI_MicroblockConfig.java
@@ -1,44 +1,44 @@
 package codechicken.multipart.nei;
 
-/* Commented until NEI is setup with maven
-
+import codechicken.lib.inventory.InventoryUtils;
 import codechicken.microblock.MicroblockClass;
 import codechicken.microblock.MicroblockClassRegistry;
+import codechicken.microblock.handler.MicroblockProxy;
+import codechicken.nei.ItemStackMap;
+import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
+import net.minecraft.util.StatCollector;
+
+import java.util.Arrays;
 
 public class NEI_MicroblockConfig implements IConfigureNEI
 {
     @Override
-    public void loadConfig()
-    {
+    public void loadConfig() {
         MicroblockClass[] microClasses = MicroblockClassRegistry.classes();
-        for(int c = 0; c < microClasses.length; c++)
-        {
+        for (int c = 0; c < microClasses.length; c++) {
             MicroblockClass mcrClass = microClasses[c];
-            if(mcrClass == null)
+            if (mcrClass == null)
                 continue;
 
-            addSubset(mcrClass, c<<8|1);
-            addSubset(mcrClass, c<<8|2);
-            addSubset(mcrClass, c<<8|4);
+            addSubset(mcrClass, c << 8 | 1);
+            addSubset(mcrClass, c << 8 | 2);
+            addSubset(mcrClass, c << 8 | 4);
         }
     }
 
-    private void addSubset(MicroblockClass mcrClass, int i)
-    {
-        API.addSetRange("Microblocks."+LangUtil.translateG(mcrClass.getName()+"."+(i&0xFF)+".subset"),
-                new MultiItemRange().add(MicroblockProxy.itemMicro(), i, i));
+    private void addSubset(MicroblockClass mcrClass, int i) {
+        API.addSubset("Microblocks." + StatCollector.translateToLocal(mcrClass.getName() + "." + (i & 0xFF) + ".subset"),
+                Arrays.asList(InventoryUtils.newItemStack(MicroblockProxy.itemMicro(), 1, i, ItemStackMap.WILDCARD_TAG)));
     }
 
     @Override
-    public String getName()
-    {
+    public String getName() {
         return "ForgeMultipart";
     }
 
     @Override
-    public String getVersion()
-    {
+    public String getVersion() {
         return "1.0.0.0";
     }
-}*/
+}
